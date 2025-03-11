519aaabd9cba5e042b4075e797a6e5b6994f2cd5
Lucas-ReyT <130539523+Lucas-ReyT@users.noreply.github.com>
2025-03-11 11:53:58 +0100

    Update App.js

--- front/src/App.js
+++ front/src/App.js
@@ -1,45 +1,50 @@
-// src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Mise à jour pour v6
-import HomePage from './pages/Home/HomePage';
-import LoginPage from './pages/Login/LoginPage';
-import SignUpPage from './pages/SignUp/SignUpPage';
-import './App.css';
+  // src/App.js
+  import React from 'react';
+  import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+  import HomePage from './pages/Home/HomePage';
+  import LoginPage from './pages/Login/LoginPage';
+  import SignUpPage from './pages/SignUp/SignUpPage';
+  import './App.css';
+  import { Link } from 'react-router-dom'; // Importer Link
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <header className="app-header">
-          <div className="logo">IPSSI</div>
-          <div className="search-bar">
-            <input type="text" placeholder="Rechercher sur Tweeter" />
-          </div>
-          <div className="user-menu">
-            <img src="https://via.placeholder.com/40" alt="User" />
-          </div>
-        </header>
-        <main className="app-content">
-          <nav className="main-nav">
-            <ul>
-              <li className="active"><i className="fa fa-home"></i> Accueil</li>
-              <li><i className="fa fa-bell"></i> Notifications</li>
-              <li><i className="fa fa-envelope"></i> Messages</li>
-              <li><i className="fa fa-bookmark"></i> Signets</li>
-              <li><i className="fa fa-user"></i> Profil</li>
-            </ul>
-          </nav>
-          <div className="main-content">
-            <Routes>
-              <Route path="/" element={<HomePage />} />  {/* Utilisation de `element` */}
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/signup" element={<SignUpPage />} />
-            </Routes>
-          </div>
-        </main>
-      </div>
-    </Router>
-  );
-}
+  function App() {
+    return (
+      <Router>
+        <div className="App">
+          <header className="app-header">
+            <div className="logo">IPSSI</div>
+            <div className="search-bar">
+              <input type="text" placeholder="Rechercher sur Tweeter" />
+            </div>
+            <div className="user-menu">
+              {/* Lien vers la page de connexion */}
+              <Link to="/login">
+                <img src="https://via.placeholder.com/40" alt="User" />
+              </Link>
+            </div>
+          </header>
+          <main className="app-content">
+            <nav className="main-nav">
+              <ul>
+                <li className="active"><i className="fa fa-home"></i> Accueil</li>
+                <li><i className="fa fa-bell"></i> Notifications</li>
+                <li><i className="fa fa-envelope"></i> Messages</li>
+                <li><i className="fa fa-bookmark"></i> Signets</li>
+                <li><i className="fa fa-user"></i> Profil</li>
+              </ul>
+            </nav>
+            <div className="main-content">
+              {/* Utilisation de Routes pour la navigation */}
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/signup" element={<SignUpPage />} />
+              </Routes>
+            </div>
+          </main>
+        </div>
+      </Router>
+    );
+  }
 
-export default App;
+  export default App;
