755f06cf049ec5912e48c1dd6730a8ec15d22764
noahrd0 <no.radal1203@gmail.com>
2025-03-11 11:27:49 +0100

    mkdir backend mv * backend/

--- run.sh
+++
@@ -1,7 +0,0 @@
-docker network create hackathon_twitter_like || true
-if ! docker run -d --rm --name db-container --network hackathon_twitter_like --network-alias db-container mongo; then
-    echo "Error: Failed to start MongoDB container"
-    exit 1
-fi
-docker build -t backend-container .
-docker run -it --rm -p 5201:3000 --name express-app --network hackathon_twitter_like --network-alias backend --mount type=bind,source="$(pwd)"/,target=/app backend-container
